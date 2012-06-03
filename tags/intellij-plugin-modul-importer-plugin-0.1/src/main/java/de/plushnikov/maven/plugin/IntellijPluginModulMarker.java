acbd4758dbe003ab374d0b73fa1c3b61ffb23a55
mplushnikov@gmail.com <mplushnikov@gmail.com@61ddf45e-e2d1-3b83-8a49-122c1660b06c>
2012-06-03 17:45:35 +0000

    [maven-release-plugin]  copy for tag intellij-plugin-modul-importer-plugin-0.1

---
+++ tags/intellij-plugin-modul-importer-plugin-0.1/src/main/java/de/plushnikov/maven/plugin/IntellijPluginModulMarker.java
@@ -0,0 +1,31 @@
+package de.plushnikov.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+
+/**
+ * Goal which does nothing, just a marker for IntelliJ Maven Importer
+ *
+ * @goal run
+ * @phase process-sources
+ */
+public class IntellijPluginModulMarker extends AbstractMojo {
+
+  /**
+   * Path to plugin META-INF directory
+   * @parameter default-value="${project.basedir}/src/main/resources"
+   */
+  private File pathToMetaInfDirectory;
+
+  /**
+   * Path to userManifest file
+   * @parameter
+   */
+  private File pathToUserManifest;
+
+  public void execute() throws MojoExecutionException {
+    //do nothing
+  }
+}
